ee4a94187ee72b906dcfbe3dc6126db3f33cd9ea
eribertoo_nucTumbleweed <erimendz@gmail.com>
2021-07-22 17:27:07 +0300

    add practice codes to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// compilation of rust code snippets
+// from book The Complete Rust Programming Ref Guide
+
+// import syntax with use keyword for library module std
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let name = env::args().skip(1).next();
+	match name {
+		Some(n) => println!("Hi there! {}", n),
+		//None => panic!("Didnt receive any name?")
+		None => println!("Didnt receive any name?")
+	}
+	let mut target = "world";
+	let mut greeting = "Hello";
+	println!("{}, {}", greeting, target);
+	// this gives error without mut kw above
+	target = "mate";
+	greeting = "How are you doing";
+	println!("{}, {}", greeting, target);
+
 }
